8bd7e964016744e779ecce27f1f1213c286f35ce
wang53224 <wang53224@126.com>
2020-07-07 22:40:03 +0800

    最初版-没有用面向对象的思想

---
+++ src/简单工厂/计算器/edition1/Calculator.java
@@ -0,0 +1,37 @@
+package 简单工厂.计算器.edition1;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.print("请输入数字A：");
+        Scanner scanner = new Scanner(System.in);
+        double numberA = scanner.nextDouble();
+        System.out.print("请输入运算符：");
+        String strOperate = scanner.next();
+        System.out.print("请输入数字B：");
+        double numberB = scanner.nextDouble();
+        double result = 0.0d;
+
+        switch (strOperate){
+            case "+":
+                result = numberA + numberB;
+                break;
+            case "-":
+                result = numberA - numberB;
+                break;
+            case "*":
+                result = numberA * numberB;
+                break;
+            case "/":
+                try {           //这里似乎没有必要
+                    result = numberA / numberB;
+                }catch (ArithmeticException e){
+                    e.printStackTrace();
+                    System.out.println("除数不能为零！");
+                }
+                break;
+        }
+        System.out.println("结果是：" + result);
+    }
+}
