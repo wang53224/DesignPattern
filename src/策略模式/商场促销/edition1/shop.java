7afd2ef7428843a96853e02b428765cbd49873b1
wang53224 <wang53224@126.com>
2020-07-19 13:19:51 +0800

    基础代码,增加InputMismatchException的处理

--- src/策略模式/商场促销/edition1/shop.java
+++ src/策略模式/商场促销/edition1/shop.java
@@ -1,5 +1,6 @@
 package 策略模式.商场促销.edition1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class shop {
@@ -9,12 +10,23 @@ public class shop {
         int num;        //数量
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("请输入商品单价：");
-        price = scanner.nextDouble();
-        System.out.print("请输入商品数量：");
-        num = scanner.nextInt();
+        boolean isTrue = true;
 
-        total = price * num;
-        System.out.println("总计价格为：" + total);
+        while (isTrue) {
+            try {
+                System.out.print("请输入商品单价：");
+                price = scanner.nextDouble();
+                System.out.print("请输入商品数量：");
+                num = scanner.nextInt();
+                total = price * num;
+                System.out.println("总计价格为：" + total);
+                isTrue = false;
+            } catch (InputMismatchException e) {
+                System.out.println("输入的数字有误，请重新输入！");
+                scanner.nextLine();// 清空输入的数字
+            }
+        }
+
+
     }
 }
