bae94f6b775f140d713b910c6678d5672cd9e64e
wang53224 <wang53224@126.com>
2020-07-19 13:36:12 +0800

    基础代码,多次计算

--- src/策略模式/商场促销/edition1/shop.java
+++ src/策略模式/商场促销/edition1/shop.java
@@ -5,26 +5,31 @@ import java.util.Scanner;
 
 public class shop {
     public static void main(String[] args) {
-        double total;   //总价
+        double total;   //单品总价
         double price;   //单价
         int num;        //数量
+        double totalPrice = 0;  //一共总价
 
+        System.out.print("输入要结算的商品总数：");
         Scanner scanner = new Scanner(System.in);
-        boolean isTrue = true;
+        int input = scanner.nextInt();
 
-        while (isTrue) {
-            try {
-                System.out.print("请输入商品单价：");
-                price = scanner.nextDouble();
-                System.out.print("请输入商品数量：");
-                num = scanner.nextInt();
-                total = price * num;
-                System.out.println("总计价格为：" + total);
-                isTrue = false;
-            } catch (InputMismatchException e) {
-                System.out.println("输入的数字有误，请重新输入！");
-                scanner.nextLine();// 清空输入的数字
+        for (int i = 0; i < input; i++) {
+            while (true) {
+                try {
+                    System.out.printf("请输入第%d件商品单价：",i+1);
+                    price = scanner.nextDouble();
+                    System.out.printf("请输入第%d件商品数量：",i+1);
+                    num = scanner.nextInt();
+                    total = price * num;
+                    totalPrice += total;
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("输入的数字有误，请重新输入！");
+                    scanner.nextLine();// 清空输入的数字
+                }
             }
+            System.out.println("->当前总计价格为：" + totalPrice);
         }
 
 
